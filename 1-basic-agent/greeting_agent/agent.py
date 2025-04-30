db9f643c591cc886d7077f1116d78802168e405e
Brandon Hancock <brandon@brandonhancock.io>
2025-04-30 13:12:42 -0400

    additional info

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="greeting_agent",
+    # https://ai.google.dev/gemini-api/docs/models
     model="gemini-2.0-flash",
     description="Greeting agent",
     instruction="""
